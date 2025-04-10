d449cb9ce1be0a86bbb4e6280e012368627540d1
Maciej Brylka <maciejbrylka94@gmail.com>
2025-04-10 21:50:09 +0100

    small improvements

--- task_3_ui_tests/pages/careers_page.py
+++ task_3_ui_tests/pages/careers_page.py
@@ -3,10 +3,12 @@ from selenium.webdriver.common.by import By
 
 class CareersPage(BasePage):
 
-    teams_xpath = (By.XPATH, "//h3[text()[contains(., 'Find your calling')]]")
+    teams_section_xpath = (By.XPATH, "//section[@id='career-find-our-calling']")
+    locations_section_xpath = (By.XPATH, "//section[@id='career-our-location']")
     life_at_insider_xpath = (By.XPATH, "//h2[text()[contains(., 'Life at Insider')]]")
 
     def verify_page_is_loaded(self):
         assert "https://useinsider.com/careers" in self.get_current_url().lower()
-        assert self.is_visible(*self.teams_xpath)
+        assert self.is_visible(*self.teams_section_xpath)
+        assert self.is_visible(*self.locations_section_xpath)
         assert self.is_visible(*self.life_at_insider_xpath)
--- task_4_api_tests/tests/test_pet_store_positive.py
+++ task_4_api_tests/tests/test_pet_store_positive.py
@@ -9,8 +9,18 @@ def test_add_pet(pet, pet_service):
     assert response.json()['category'] == pet.category
     assert response.json()['photoUrls'] == pet.photoUrls
     assert response.json()['tags'] == pet.tags
+    
+    get_response = pet_service.find_pet_by_id(pet.id)
+    assert get_response.status_code == 200
+    assert get_response.json()['name'] == pet.name
+    assert get_response.json()['status'] == pet.status
+    assert get_response.json()['id'] == pet.id
+    assert get_response.json()['category'] == pet.category
+    assert get_response.json()['photoUrls'] == pet.photoUrls
+    assert get_response.json()['tags'] == pet.tags
 
 def test_update_pet(pet, pet_service):
+    pet.name = "Updated Name"
     response = pet_service.update_pet(pet)
     assert response.status_code == 200
 
