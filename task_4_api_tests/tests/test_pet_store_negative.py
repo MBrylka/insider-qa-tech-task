c8a8585b235adc827e88c4b1a273e8c370484b39
Maciej Brylka <maciejbrylka94@gmail.com>
2025-04-10 10:35:00 +0100

    improve negative api tests

--- task_4_api_tests/tests/test_pet_store_negative.py
+++ task_4_api_tests/tests/test_pet_store_negative.py
@@ -2,12 +2,16 @@ import pytest
 
 
 # POST endpoint
-def test_add_pet_invalid_data(pet_service):
+def test_add_pet_empty_data(pet_service):
     response = pet_service.add_pet(None)
     assert response.status_code == 405
 
-def test_add_pet_broken_json(pet_service):
+def test_add_pet_empty_data(pet_service):
     response = pet_service.add_pet({"id": "text"})
+    assert response.status_code == 405
+
+def test_add_pet_broken_json(pet_service):
+    response = pet_service.add_pet("text")
     assert response.status_code == 400
 
 
@@ -17,17 +21,17 @@ def test_update_non_existing_pet(invalid_pet, pet_service):
     assert response.status_code == 404
 
 def test_update_invalid_data(pet_service):
-    response = pet_service.update_pet(None)
+    response = pet_service.update_pet({"id": "text"})
     assert response.status_code == 405
 
 def test_update_pet_broken_json(pet_service):
-    response = pet_service.update_pet({"id": "text"})
+    response = pet_service.update_pet("text")
     assert response.status_code == 400
 
 
 # GET endpoint
 def test_get_non_existing_pet(pet_service):
-    response = pet_service.find_pet_by_id(999999)
+    response = pet_service.find_pet_by_id(999999) # Assuming this ID does not exist, petStore seems to return random values
     assert response.status_code == 404
 
 def test_get_invalid_id(pet_service):
@@ -37,7 +41,7 @@ def test_get_invalid_id(pet_service):
 
 # DELETE endpoint
 def test_delete_non_existing_pet(pet_service):
-    response = pet_service.delete_pet(999999)
+    response = pet_service.delete_pet(999999) # the same here, random values returned from pet store
     assert response.status_code == 404
 
 def test_delete_pet_invalid_id(pet_service):
