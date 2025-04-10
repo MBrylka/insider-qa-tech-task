d4e0bb19cde4335f682b91001274d621a750acde
Maciej Brylka <maciejbrylka94@gmail.com>
2025-04-10 19:31:32 +0100

    add screenshot to html report

--- task_3_ui_tests/conftest.py
+++ task_3_ui_tests/conftest.py
@@ -1,9 +1,26 @@
 import pytest
+import pytest_html
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    if report.when == "call" and report.failed:
+        driver = item.funcargs.get("driver", None)
+        if driver:
+            os.makedirs("screenshots", exist_ok=True)
+            screenshot_path = f"screenshots/{item.name}.png"
+            driver.save_screenshot(screenshot_path)
+            extra = getattr(report, "extra", [])
+            extra.append(pytest_html.extras.image(screenshot_path))
+            report.extra = extra
 
 @pytest.fixture
 def driver(browser):
